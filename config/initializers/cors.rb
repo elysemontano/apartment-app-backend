249de0e98fa667e1abc33bef624fbde2493117b2
Elyse Montano <elysemontano.dev.com>
2023-05-02 15:16:11 -0700

    updates cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://apartment-app-frontend.onrender.com/'
+    origins '*'
     resource '*',
     headers: ["Authorization"],
     expose: ["Authorization"],
